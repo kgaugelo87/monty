c6119498d3ad0f7f8789c2371c4b50912a052511
kgaugelo87 <kgaugelo.maile@gmail.com>
2024-04-03 01:49:27 -0700

    Implement the pint opcode

--- monty.h
+++ monty.h
@@ -39,7 +39,7 @@ ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
+void pint(stack_t **head, unsigned int number);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
 void f_pop(stack_t **head, unsigned int counter);
---
+++ pint.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * pint - the value at the top of the stack
+ * @head: our head stack
+ * @number: our stack number
+ * Return: stack and if stack empty print error
+ *
+ */
+
+void pint(stack_t **head, unsigned int number)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
