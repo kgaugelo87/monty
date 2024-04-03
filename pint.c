e3af09af6f14689898a214457363ac815173737c
kgaugelo87 <kgaugelo.maile@gmail.com>
2024-04-03 02:44:01 -0700

    Implement the push and pall opcodes

--- pint.c
+++ pint.c
@@ -10,13 +10,12 @@
 
 void pint(stack_t **head, unsigned int number)
 {
-	if (*head == NULL)
+	(void) stack;
+	if (arguments->head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", number);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
+		free_all_args();
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*head)->n);
+	printf("%d\n", arguments->head->n);
 }
